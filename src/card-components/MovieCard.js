167cec5fd53a14a2d005343be48e79cbfe959332
Rafael <RafaelB24@yahoo.com>
2019-07-27 19:44:55 -0400

    Done.

--- src/card-components/MovieCard.js
+++ src/card-components/MovieCard.js
@@ -40,4 +40,17 @@ export default class MovieCard extends Component {
   }
 }
 
-// Don't forget your default props!
+MovieCard.defaultProps = {
+  title: "Unknown",
+  IMDBRating: null,
+  genres: ["No Genre(s) Found"],
+  poster: "default"
+
+}
+
+
+//
+//
+//
+//
+//
