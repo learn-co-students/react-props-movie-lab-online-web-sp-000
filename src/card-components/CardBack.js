b6078b46d03ca2b7c703217ebf16c57523d9a58f
Mark <m.hern1415@gmail.com>
2020-05-05 15:18:38 -0700

    Done.

--- src/card-components/CardBack.js
+++ src/card-components/CardBack.js
@@ -10,18 +10,30 @@ const imgMapper = {0: zero, 1: one, 2: two, 3: three, 4: four, 5: five}
 
 export default class CardBack extends Component {
 
-  generateRatingElement = () => {
+  generateRatingElement = (rating) => {
     // implement meeeee! See the readme for instructions
+    if(rating == null){
+      return <h4>{'No Rating Found'}</h4>
+    } 
+    return <img src ={imgMapper[rating]} alt="huh?" />
+  }
+
+  generateGenres = (genres) => {
+    if(genres.length === 0){
+      return undefined 
+    } 
+    return genres.map((genre, i) => <li key={i}>{genre + ", "}</li>)
   }
 
   render() {
     return (
       <div className="card-back">
-        <h3 className="title"></h3>
+        <h3 className="title">{this.props.title}</h3>
         <span />
         { /* your rating element should go here -- you can invoke methods within JSX, à la: this.myMethod() */ }
+        {this.generateRatingElement(this.props.IMDBRating)}
         <span />
-        <h5 className="genres"></h5>
+        <h5 className="genres">{this.generateGenres(this.props.genres)}</h5>
       </div>
     )
   }
