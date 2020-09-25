75e36760512b99f7689a790cf2921134ab90d697
github username <github email address>
2020-09-24 21:35:28 -0400

    Done.

--- src/MovieShowcase.js
+++ src/MovieShowcase.js
@@ -6,6 +6,8 @@ export default class MovieShowcase extends Component {
 
   generateMovieCards = () => {
     // map over your movieData array and return an array of the correct JSX
+    return movieData.map((movieObj, i) => <MovieCard key = {i} poster={movieObj.poster} title={movieObj.title} IMDBRating = {movieObj.IMDBRating} genres={movieObj.genres}/>)
+
   }
 
   render() {
