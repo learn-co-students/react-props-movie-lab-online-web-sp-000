e93032567d24613cfe95746d470436b6c0cbb277
Christopher Dent <christopherdent01@gmail.com>
2020-10-14 19:55:41 -0400

    Done.

--- src/MovieShowcase.js
+++ src/MovieShowcase.js
@@ -5,10 +5,13 @@ import movieData from './data.js'
 export default class MovieShowcase extends Component {
 
   generateMovieCards = () => {
-    // map over your movieData array and return an array of the correct JSX
+      return movieData.map((movie) =>
+       <MovieCard title={movie.title} IMDBRating={movie.IMDBRating} genres ={movie.genres} poster={movie.poster} />)
+    // map over your movieData array and return an array of the correct JSX Wrote out array passing in data from movieData. now these become props
   }
 
   render() {
+
     return (
       <div id="movie-showcase">
         {this.generateMovieCards()}
--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import MovieShowcase from './MovieShowcase';
-
+  
 ReactDOM.render(<MovieShowcase />, document.getElementById('root'));
