a5093e207c8051f616708e905a4b5c455d625c27
ting682 <tchung682@gmail.com>
2020-10-27 09:01:01 -0400

    Done.

--- src/card-components/CardBack.js
+++ src/card-components/CardBack.js
@@ -12,16 +12,24 @@ export default class CardBack extends Component {
 
   generateRatingElement = () => {
     // implement meeeee! See the readme for instructions
+    if (this.props.IMDBRating === null) {
+      return 'no rating found'
+    } else {
+      return ( 
+        <img src={imgMapper[`${this.props.IMDBRating}`]} />
+      )
+    }
   }
 
   render() {
     return (
       <div className="card-back">
-        <h3 className="title"></h3>
+        <h3 className="title">{this.props.title}</h3>
         <span />
         { /* your rating element should go here -- you can invoke methods within JSX, à la: this.myMethod() */ }
+        <h4 className="IMDBRating">{this.generateRatingElement()}</h4>
         <span />
-        <h5 className="genres"></h5>
+        <h5 className="genres">{this.props.genres.join(', ')}</h5>
       </div>
     )
   }
--- src/card-components/MovieCard.js
+++ src/card-components/MovieCard.js
@@ -30,14 +30,21 @@ const posterMap = {
 export default class MovieCard extends Component {
 
   render() {
+    //debugger
     return (
       <div className="movie-card">
         {/* which component should receive which props? */}
-        <CardFront />
-        <CardBack />
+        <CardFront poster={posterMap[`${this.props.poster}`]}/>
+        <CardBack IMDBRating={this.props.IMDBRating} title={this.props.title} genre={this.props.genres}/>
       </div>
     )
   }
 }
 
 // Don't forget your default props!
+MovieCard.defaultProps = {
+  poster: 'default',
+  title: 'Unknown',
+  IMDBRating: null,
+  genres: ['No Genre(s) Found']
+}
