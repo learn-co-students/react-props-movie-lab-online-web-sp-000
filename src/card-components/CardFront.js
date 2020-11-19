1f36e54726bc989e4337e996964027fed8640207
jessicagomesng <jgnpeluso@gmail.com>
2020-11-19 12:20:47 +0000

    Done.

--- src/card-components/CardFront.js
+++ src/card-components/CardFront.js
@@ -3,8 +3,11 @@ import React, { Component } from 'react';
 export default class CardFront extends Component {
 
   render() {
+    const img = `${this.props.poster}`;
+
+    console.log(img);
     return (
-      <div className="card-front" style={{backgroundImage: `url(${''})`}}>
+      <div className="card-front" style={{backgroundImage: `url(${img})`}}>
       </div>
     )
   }
