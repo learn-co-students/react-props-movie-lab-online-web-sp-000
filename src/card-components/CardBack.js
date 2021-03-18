b64d8c8d5aede5fba33dc8b872bf836fbe12f775
keelsk <keelsk@gmail.com>
2021-03-18 09:37:25 -0400

    Done.

--- src/card-components/CardBack.js
+++ src/card-components/CardBack.js
@@ -12,8 +12,8 @@ export default class CardBack extends Component {
 
   generateRatingElement = () => {
     // implement meeeee! See the readme for instructions
-    if (this.props.IMDBRating){
-      return <img src = {imgMapper[this.props.IMDBRating]} />
+    if (this.props.IMDBRating !== null){
+      return <img src = {imgMapper[this.props.IMDBRating]} alt=""/>
     } else{
       return <h4>No Rating Found</h4>
     }
