5c82d4995d466cfd9bbc938de5dccebae7c58a2a
nlanese21 <nlanese21@gmail.com>
2021-05-12 17:33:19 -0400

    Done.

--- src/card-components/CardBack.js
+++ src/card-components/CardBack.js
@@ -11,7 +11,17 @@ const imgMapper = {0: zero, 1: one, 2: two, 3: three, 4: four, 5: five}
 export default class CardBack extends Component {
 
   generateRatingElement = () => {
-    // implement meeeee! See the readme for instructions
+    let rating = this.props.IMDBRating
+    if (rating == null){
+      return(<h4>No Rating Found</h4>)
+    } 
+    else 
+     return(<img src= {imgMapper[rating]} alt = "" />)
+  }
+
+  generateGenreElement = () => {
+    genres = this.props.genres
+    return genres.map(item => <p>{item}</p>)
   }
 
   render() {
@@ -19,10 +29,14 @@ export default class CardBack extends Component {
       <div className="card-back">
         <h3 className="title"></h3>
         <span />
-        { /* your rating element should go here -- you can invoke methods within JSX, à la: this.myMethod() */ }
+        <h4>{this.props.title}</h4>
+        {this.generateRatingElement}
         <span />
-        <h5 className="genres"></h5>
+        <h5 className="genres">
+          {this.generateGenreElement}
+        </h5>
       </div>
     )
   }
 }
+
--- src/card-components/MovieCard.js
+++ src/card-components/MovieCard.js
@@ -29,15 +29,28 @@ const posterMap = {
 
 export default class MovieCard extends Component {
 
+  getActualImg = () => {
+    return posterMap[`${this.props.poster}`]
+  }
+
   render() {
     return (
       <div className="movie-card">
         {/* which component should receive which props? */}
-        <CardFront />
-        <CardBack />
+        <CardFront poster = {this.getActualImg()}/>
+        <CardBack title = {this.props.title} IMDBRating = {this.props.IMDBRating} genres = {this.props.genres} />
       </div>
     )
   }
 }
 
 // Don't forget your default props!
+CardBack.defaultProps = {
+  title: "Unknown",
+  IMDBRating: null,
+  genres: ["No Genre(s) Found"]
+}
+
+CardFront.defaultProps = {
+  poster: "default"
+}
